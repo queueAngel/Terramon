f0abdade5d9a641ca9fbec430d2b32296f64243c
queue <angeltrisix@gmail.com>
2025-11-18 18:13:16 -0500

    idk what this is

---
+++ Terramon/Content/Visuals/ParticleEmitter.cs
@@ -0,0 +1,114 @@
+﻿using ReLogic.Content;
+using System.Runtime.InteropServices;
+
+namespace Terramon.Content.Visuals;
+
+public sealed class ParticleEmitter(ParticleSchema schema, Asset<Texture2D> tex)
+{
+    public ParticleSchema Schema = schema;
+    public Asset<Texture2D> Texture = tex;
+    public byte TimeLeft;
+    public bool KeptAlive;
+    public bool Additive;
+    public List<Particle> Particles = [];
+    public List<Vector2> Trails = schema.Trail.Style != ParticleTrailStyle.None ? [] : null;
+    public int Emit(Vector2 position, Vector2 velocity, float rotation = 0f, byte lifetime = 60)
+    {
+        if (Main.dedServ)
+            return 0;
+        Particle newP = new()
+        {
+            Position = position,
+            Velocity = velocity,
+            Rotation = rotation,
+            TimeLeft = lifetime,
+            SpawnParameters = new(lifetime)
+        };
+        int idx = 0;
+        foreach (ref var p in CollectionsMarshal.AsSpan(Particles))
+        {
+            if (p.TimeLeft == 0)
+            {
+                p = newP;
+                return idx;
+            }
+            idx++;
+        }
+        idx = Particles.Count;
+        Particles.Add(newP);
+        return idx;
+    }
+    public void UpdateParticles()
+    {
+        if (KeptAlive)
+        {
+            TimeLeft = 2;
+            KeptAlive = false;
+        }
+        else
+        {
+            if (Particles.Count != 0)
+                TimeLeft = Math.Max(TimeLeft, Particles.Max(p => p.TimeLeft));
+        }
+        foreach (ref var p in CollectionsMarshal.AsSpan(Particles))
+        {
+            if (p.TimeLeft == 0)
+                continue;
+            AI(ref p);
+            p.Position += p.Velocity;
+            p.Rotation += p.AngularVelocity;
+        }
+    }
+    public void AI(ref Particle p)
+    {
+
+    }
+    public Color GetAlpha(in Particle p) => Lighting.GetColor(p.Position.ToTileCoordinates());
+}
+
+public struct ParticleTrailDrawer(ParticleTrail schema)
+{
+    public ParticleTrail Schema = schema;
+    public Vector2[] Positions = new Vector2[schema.Length];
+    public void Set(Vector2 allPositions) => Array.Fill(Positions, allPositions);
+    public void Record(Vector2 position)
+    {
+        for (int i = Positions.Length - 2; i >= 0; i--)
+            Positions[i + 1] = Positions[i];
+        Positions[0] = position;
+    }
+}
+
+public struct Particle()
+{
+    public Vector2 Position;
+    public Vector2 Velocity;
+    public float Rotation;
+    public float AngularVelocity;
+    public float Scale = 1f;
+    public byte Opacity = byte.MaxValue;
+    public byte TimeLeft;
+    public ParticleSpawnParameters SpawnParameters;
+    public Vector2[] OldPositions;
+    public readonly void DrawCommon(
+        ParticleEmitter source,
+        SpriteBatch sb,
+        Texture2D tex,
+        Color color = default,
+        Vector2? origin = null,
+        float rotation = 0f)
+    {
+        if (tex is null)
+            return;
+
+        if (color.PackedValue == 0u)
+            color = source.GetAlpha(in this);
+        if (source.Additive)
+            color.A = 0;
+
+        var orig = origin ?? tex.Size() * 0.5f;
+        sb.Draw(tex, Position, null, color, rotation, orig, Scale, SpriteEffects.None, 0f);
+    }
+}
+
+public readonly record struct ParticleSpawnParameters(byte TimeLeft);
---
+++ Terramon/Content/Visuals/VisualsLoader.cs
@@ -0,0 +1,134 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Terramon.Content.Visuals;
+
+[Autoload(false)]
+public sealed class VisualsLoader : ModSystem
+{
+    public Dictionary<string, ParticleEmitter> EmittersByName = [];
+    public ParticleEmitter[] EmittersByID;
+    public override void Load()
+    {
+        // Load effect schemas from file
+        var jsonStream = Mod.GetFileStream($"Assets/Data/VFX.json");
+
+        var json = JsonDocument.Parse(jsonStream);
+
+        var emitters = new List<ParticleEmitter>();
+        foreach (var particle in json.RootElement.GetProperty("particles").EnumerateObject())
+        {
+            var name = particle.Name;
+            var body = particle.Value;
+
+            ParticleProperties props;
+            ParticleMovement mov;
+            ParticleTrail tr;
+
+            if (body.TryGetProperty("properties", out var properties))
+                props = JsonSerializer.Deserialize<ParticleProperties>(properties);
+            else
+                props = new();
+
+            if (body.TryGetProperty("movement", out var movement))
+                mov = JsonSerializer.Deserialize<ParticleMovement>(movement);
+            else
+                mov = new();
+
+            if (body.TryGetProperty("trail", out var trail))
+                tr = JsonSerializer.Deserialize<ParticleTrail>(trail);
+            else
+                tr = new();
+
+            var schema = new ParticleSchema(props, mov, tr);
+            var emitter = new ParticleEmitter(schema, null);
+            emitters.Add(emitter);
+            EmittersByName.Add(name, emitter);
+        }
+        EmittersByID = emitters.ToArray();
+    }
+    public override void Unload()
+    {
+        EmittersByName = null;
+        EmittersByID = null;
+    }
+}
+
+public sealed record ParticleSchema(
+    ParticleProperties Properties = null,
+    ParticleMovement Movement = null,
+    ParticleTrail Trail = null);
+public sealed record ParticleProperties(bool Pixelated = false, byte FadeIn = 0, byte FadeOut = 0);
+public sealed record ParticleMovement(
+    ParticleMovementStyle Style = ParticleMovementStyle.None,
+    ParticlePathShape PathShape = ParticlePathShape.Line,
+    float Start = 0f, float End = 1f);
+public sealed record ParticleTrail(
+    ParticleTrailStyle Style = ParticleTrailStyle.None,
+    byte Length = 0, float Size = 0f,
+    [property: JsonConverter(typeof(ColorParser))] Color Color = default,
+    ParticleTrailTaper Taper = ParticleTrailTaper.None,
+    Ease TaperFunction = Ease.None);
+public sealed class ColorParser : JsonConverter<Color>
+{
+    private static Dictionary<uint, string> ColorNames = [];
+    public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.String)
+            throw new InvalidOperationException();
+        var str = reader.GetString().TrimStart('#');
+        if (uint.TryParse(str, System.Globalization.NumberStyles.HexNumber, null, out var packed))
+            return new Color(packed | (255u << 24));
+
+        var getter = typeof(Color).GetMethod("get_" + str, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+        if (getter != null)
+            return (Color)getter.Invoke(null, null);
+        throw new InvalidOperationException();
+    }
+    public override void Write(Utf8JsonWriter writer, Color value, JsonSerializerOptions options)
+    {
+        if (ColorNames is null)
+        {
+            ColorNames = [];
+            var props = typeof(Color).GetProperties(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            for (int i = 0; i < props.Length; i++)
+            {
+                var prop = props[i];
+                var color = (Color)prop.GetValue(null, null);
+                ColorNames.Add(color.PackedValue, prop.Name);
+            }
+        }
+        if (ColorNames.TryGetValue(value.PackedValue, out var name))
+            writer.WriteStringValue(name);
+        else
+            writer.WriteStringValue("#" + value.PackedValue.ToString("X6"));
+    }
+}
+public enum ParticleMovementStyle : byte
+{
+    None,
+    TracePath,
+    TracePathLoop,
+    TracePathPingPong,
+}
+public enum ParticlePathShape : byte
+{
+    Line,
+    BezierCurve,
+    Circle,
+    Triangle,
+    Square,
+}
+public enum ParticleTrailStyle : byte
+{
+    None,
+    AfterImages,
+    Mesh,
+}
+public enum ParticleTrailTaper : byte
+{
+    None,
+    Both,
+    Target,
+    Start,
+}
